38ee4873c195efc4bb5ec14e591e2beb52e78ffc
Roman Shainov <roman.shaynov@mego.travel>
2025-04-19 23:39:08 +0300

    Use pointer instead of value slice

--- internal/repository/storeVehicle.go
+++ internal/repository/storeVehicle.go
@@ -8,21 +8,21 @@ import (
 )
 
 var (
-	vehicles []vehicle.Vehicle
-	cars     []car.Car
-	motos    []moto.Moto
+	vehicles []*vehicle.Vehicle
+	cars     []*car.Car
+	motos    []*moto.Moto
 )
 
 func StoreVehicle(v vehicle.GenericVehicle) {
 	switch v := v.(type) {
 	case *car.Car:
-		cars = append(cars, *v)
+		cars = append(cars, v)
 		fmt.Printf("Stored car: %+v\n", v.GetGeneralInfo())
 	case *moto.Moto:
-		motos = append(motos, *v)
+		motos = append(motos, v)
 		fmt.Printf("Stored moto: %+v\n", v.GetGeneralInfo())
 	case *vehicle.Vehicle:
-		vehicles = append(vehicles, *v)
+		vehicles = append(vehicles, v)
 		fmt.Printf("Stored vehicle: %+v\n", v.GetGeneralInfo())
 	default:
 		fmt.Println("Unknown vehicle type")
